cf51b7f6bb1ce2c16c6cb4d145e23c7619a3a3aa
Seiichi KONDO <seikichi@kmc.gr.jp>
2014-08-08 09:34:52 +0900

    loads ruby-mode when opening *.rake files

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -367,6 +367,7 @@
 
 ;; Ruby
 (add-to-list 'auto-mode-alist '("\\.gemspec\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Capfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
