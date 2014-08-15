0285ef677840c2cf4649b18f5bcad3f546626d1c
Seiichi KONDO <seikichi@kmc.gr.jp>
2014-08-16 01:39:46 +0900

    updates smartrep & multiple-corsors mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -319,9 +319,18 @@
 (declare-function smartrep-define-key "smartrep")
 (global-unset-key "\M-q")
 (smartrep-define-key global-map "M-q"
-  '(("C-p" . 'mc/mark-previous-like-this)
-    ("C-n" . 'mc/mark-next-like-this)
-    ("*"   . 'mc/mark-all-like-this)))
+  '(("n"        . 'mc/mark-next-like-this)
+    ("p"        . 'mc/mark-previous-like-this)
+    ("m"        . 'mc/mark-more-like-this-extended)
+    ("u"        . 'mc/unmark-next-like-this)
+    ("U"        . 'mc/unmark-previous-like-this)
+    ("s"        . 'mc/skip-to-next-like-this)
+    ("S"        . 'mc/skip-to-previous-like-this)
+    ("*"        . 'mc/mark-all-like-this)
+    ("d"        . 'mc/mark-all-like-this-dwim)
+    ("i"        . 'mc/insert-numbers)
+    ("o"        . 'mc/sort-regions)
+    ("O"        . 'mc/reverse-regions)))
 
 ;; ignores cases in file completetion
 (setq completion-ignore-case t)
