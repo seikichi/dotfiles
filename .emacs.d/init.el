35e34e6d3baf0211d7fa7e92a372ce4e2b2b411c
Seiichi KONDO <seikichi@kmc.gr.jp>
2014-09-11 08:22:29 +0900

    disables flycheck-pos-tip

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -299,9 +299,6 @@
 
 ;; flycheck
 (add-hook 'after-init-hook #'global-flycheck-mode)
-(eval-after-load 'flycheck
-  '(custom-set-variables
-    '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
 
 ;; multiple-cursors & smartrep
 (require 'multiple-cursors)
