1abf0f858b3251e0f240168e85270128f4a1d7f7
Seiichi KONDO <seikichi@kmc.gr.jp>
2019-10-22 01:28:20 +0900

    Remove peco settings

--- setup.sh
+++ setup.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 DIR=$(dirname ${0})
-DOT_FILES=( .zshrc .gitconfig .tmux.conf .dir_colors .emacs.d .peco )
+DOT_FILES=( .zshrc .gitconfig .tmux.conf .dir_colors .emacs.d )
 
 for file in ${DOT_FILES[@]}
 do
     ln -s "$DIR/$file" "$HOME/$file"
 done
-
-# download
-mkdir -p ${DIR}/bin
-wget http://sourceforge.net/projects/plantuml/files/plantuml.jar/download -O ${DIR}/bin/plantuml.jar
